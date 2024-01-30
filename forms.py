712b3492cce2922f8d664aadb92cb0c7307c7c5f
IDGS-802-21000485 <156386746+IDGS-802-21000485@users.noreply.github.com>
2024-01-29 23:10:09 -0600

    AFBF_29/01/24_CalcularDistancias
    Subo los puros archivos de lo que hice ya que no puedo subir el proyecto completo ya que el repositorio no me deja subirlo, una disculpa

---
+++ forms.py
@@ -0,0 +1,9 @@
+from wtforms import Form
+from wtforms import StringField
+
+class corde(Form):
+    X1 = StringField("X1")
+    Y1 = StringField("Y1")
+
+    X2 = StringField("X2")
+    Y2 = StringField("Y2")
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, render_template
+import forms
+import math
 
 app = Flask(__name__)
 
 @app.route("/")
 def calculo():
-    return render_template("calculo.html")
+    return render_template("layout.html")
 
 @app.route("/operacion", methods=("GET", "POST"))
 def res():
@@ -30,6 +32,28 @@ def res():
             operacion = "División"
 
     return render_template("calculo.html", resultado=resultado, operacion=operacion)
+
+@app.route("/dosPuntos",methods=("GET","POST"))
+def alumnos():
+    alum_forms=forms.corde(request.form)
+    X1 = 0
+    X2 = 0
+
+    Y1 = 0
+    Y2 = 0
+
+    res = 0
+    
+    if request.method=='POST':
+        X1 = int(alum_forms.X1.data)
+        Y1 = int(alum_forms.Y1.data)
+        X2 = int(alum_forms.X2.data)
+        Y2 = int(alum_forms.Y2.data)
+
+        res = math.sqrt((X2 - X1)**2 + (Y2 - Y1)**2)
+
+    return render_template("distanciaADosPuntos.html", form=alum_forms, res = res)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
